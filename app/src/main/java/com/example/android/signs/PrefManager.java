e8f407a950b7ff6f58537b07a648e263451ed730
simplydikka <diddeto@gmail.com>
2018-11-02 16:54:59 +0200

    Followed the tutorial till the end, but app crashes on button click

---
+++ app/src/main/java/com/example/android/signs/PrefManager.java
@@ -0,0 +1,38 @@
+package com.example.android.signs;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by Lincoln on 05/05/16.
+ */
+public class PrefManager {
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+    Context _context;
+
+    // shared pref mode
+    int PRIVATE_MODE = 0;
+
+    // Shared preferences file name
+    private static final String PREF_NAME = "welcome_activity";
+
+    private static final String IS_FIRST_TIME_LAUNCH = "IsFirstTimeLaunch";
+
+    @SuppressLint("CommitPrefEdits")
+    public PrefManager(Context context) {
+        this._context = context;
+        pref = _context.getSharedPreferences(PREF_NAME, PRIVATE_MODE);
+        editor = pref.edit();
+    }
+
+    public void setFirstTimeLaunch(boolean isFirstTime) {
+        editor.putBoolean(IS_FIRST_TIME_LAUNCH, isFirstTime);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunch() {
+        return pref.getBoolean(IS_FIRST_TIME_LAUNCH, true);
+    }
+
+}
